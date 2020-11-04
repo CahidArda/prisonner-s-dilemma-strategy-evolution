021286ce9a638d8f4a26040a7e3f8e794d7f4213
CahidArda <57228345+CahidArda@users.noreply.github.com>
2020-11-04 21:32:19 +0300

    Updated the functions

--- previous_results/displayOutput.py
+++ previous_results/displayOutput.py
@@ -2,54 +2,65 @@ import pandas as pd
 import matplotlib.pyplot as plt
 plt.style.use('ggplot')
 
+fig, axs = plt.subplots(3, 1, constrained_layout=True)
+
+def fillSubPlot(subPlotIndex, labelOffset, fromColumn, toColumn, title):
+	x = df['Generation']
+	ax = axs[subPlotIndex]
+	for column in df.columns[fromColumn:toColumn]:
+
+		ax.set_ylim([-0.05,1.05])
+		ax.set_title(title)
+		ax.plot(x, df[column].values, label=column[labelOffset:])
+		ax.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+
+
 def getBestPerformingAgentOverTime():
 	x = df['Generation']
 	for column in df.columns[1:5]:
-		
-		axes = plt.gca()
-		axes.set_ylim([0,1])
-		plt.subplot(311)
-		plt.plot(x, df[column].values, label=column)
-		plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+
+		axs[0].set_ylim([-0.05,1.05])
+		axs[0].set_title("Best Performing Agent")
+		axs[0].plot(x, df[column].values, label=column[5:])
+		axs[0].legend(loc='center left', bbox_to_anchor=(1, 0.5))
 
 
 def getWorstPerformingAgentOverTime():
 	x = df['Generation']
 	for column in df.columns[5:9]:
 
-		axes = plt.gca()
-		axes.set_ylim([0,1])
-		plt.subplot(312)
-		plt.plot(x, df[column].values, label=column)
-		plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+		axs[1].set_ylim([-0.05,1.05])
+		axs[1].set_title("Worst Performing Agent")
+		axs[1].plot(x, df[column].values, label=column[5:])
+		axs[1].legend(loc='center left', bbox_to_anchor=(1, 0.5))
 
 
 def getAverageAgentOverTime():
 	x = df['Generation']
 	for column in df.columns[9:13]:
 		
-		axes = plt.gca()
-		axes.set_ylim([0,1])
-		plt.subplot(313)
-		plt.plot(x, df[column].values, label=column)
-		plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+		axs[2].set_ylim([-0.05,1.05])
+		axs[2].set_title("Average of All Agents")
+		axs[2].plot(x, df[column].values, label=column[4:])
+		axs[2].legend(loc='center left', bbox_to_anchor=(1, 0.5))
 
 realTime = False
-fileCode = "r3"
+fileCode = input("Enter file: ")
 fileName = "{}/generationDetailsOutput.csv".format(fileCode)
+
 
 if realTime:
 	for i in range(1000):
 		df = pd.read_csv(fileName)
-		getBestPerformingAgentOverTime()
-		getWorstPerformingAgentOverTime()
-		getAverageAgentOverTime()	
+		fillSubPlot(0, 5, 1, 5, "Best Performing Agent")
+		fillSubPlot(1, 5, 5, 9, "Worst Performing Agent")
+		fillSubPlot(2, 4, 9, 13, "Average of All Agents")
 		plt.pause(0.5)
 		plt.clf()
 else:
 	df = pd.read_csv(fileName)
-	getBestPerformingAgentOverTime()
-	getWorstPerformingAgentOverTime()
-	getAverageAgentOverTime()	
+	fillSubPlot(0, 5, 1, 5, "Best Performing Agent")
+	fillSubPlot(1, 5, 5, 9, "Worst Performing Agent")
+	fillSubPlot(2, 4, 9, 13, "Average of All Agents")
 
 plt.show()
